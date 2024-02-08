a990912bf5dd57b5e2b04301077587de57f203e0
anarodr2 <gokina.nae.nae@gmail.com>
2024-02-08 22:33:29 +0100

    AE3

---
+++ AE3/src/main/java/es/florida/Main.java
@@ -0,0 +1,19 @@
+package es.florida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Clase principal de la aplicación Spring Boot
+ */
+@SpringBootApplication
+public class Main {
+	/**
+	 * Método principal que se ejecuta al iniciar la aplicación
+	 * @param args Argumentos de la línea de comandos
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+
+	}
+}
