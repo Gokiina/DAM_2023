13dae32e516c17c2429a7aeca2741d8ca11e3cd2
anarodr2 <gokina.nae.nae@gmail.com>
2024-10-01 12:56:05 +0200

    games

--- CalculadoraIMC/ios/Pods/Target Support Files/Pods-CalculadoraIMC-CalculadoraIMCTests/Pods-CalculadoraIMC-CalculadoraIMCTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CalculadoraIMC_CalculadoraIMCTests : NSObject
-@end
-@implementation PodsDummy_Pods_CalculadoraIMC_CalculadoraIMCTests
-@end
--- CalculadoraIMC/ios/Pods/Target Support Files/Pods-CalculadoraIMC/Pods-CalculadoraIMC-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CalculadoraIMC : NSObject
-@end
-@implementation PodsDummy_Pods_CalculadoraIMC
-@end
